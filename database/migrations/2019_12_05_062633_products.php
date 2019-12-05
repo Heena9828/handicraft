98f04d0da0dfebf12ebdc24cbe4657f55ea8461a
Heena9828 <heenaa231@gmail.com>
2019-12-05 12:13:57 +0530

    project init

---
+++ database/migrations/2019_12_05_062633_products.php
@@ -0,0 +1,41 @@
+<?php
+
+    use Illuminate\Database\Migrations\Migration;
+    use Illuminate\Database\Schema\Blueprint;
+    use Illuminate\Support\Facades\Schema;
+
+    class Products extends Migration
+    {
+
+        /**
+         * Run the migrations.
+         *
+         * @return void
+         */
+        public function up()
+        {
+            Schema::create('products', function (Blueprint $table)
+            {
+                $table->increments('id');
+                $table->string('product_name');
+                $table->string('sku');
+                $table->integer('price');
+                $table->string('image');
+                $table->string('description');
+                $table->integer('subcategory_id')->unsigned();
+                $table->timestamps();
+            });
+        }
+
+        /**
+         * Reverse the migrations.
+         *
+         * @return void
+         */
+        public function down()
+        {
+            Schema::dropIfExists('products');
+        }
+
+    }
+    
